1d254bf3d609c9a067ffa9309fd92b85d8ba3b6b
성혜람 <wodnr624@gmail.com>
2025-10-14 20:12:11 +0900

    Initial commit

---
+++ src/main/java/com/kit/kitbot/KitbotApplication.java
@@ -0,0 +1,13 @@
+package com.kit.kitbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitbotApplication.class, args);
+	}
+
+}
